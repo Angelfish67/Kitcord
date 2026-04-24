bd8cce9160858632e6088ae1b02f49deb6959ae8
angelfish67 <devcore404@gmail.com>
2026-04-24 13:22:31 +0200

    task entity added

--- src/main/java/ch/samira/tesan/focusdesk/task/Task.java
+++ src/main/java/ch/samira/tesan/focusdesk/task/Task.java
@@ -1,4 +1,39 @@
 package ch.samira.tesan.focusdesk.task;
 
+import ch.samira.tesan.focusdesk.task.enums.TaskPriority;
+import ch.samira.tesan.focusdesk.task.enums.TaskStatus;
+import ch.samira.tesan.focusdesk.user.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tasks")
 public class Task {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(length = 100, nullable = false)
+        @Size(max = 100)
+        @NotEmpty
+        private String title;
+
+        private String description;
+
+        @Enumerated(EnumType.STRING)
+        private TaskStatus status;
+
+        @Enumerated(EnumType.STRING)
+        private TaskPriority priority;
+
+
+        public Task(){
+        }
+
+
+
 }
